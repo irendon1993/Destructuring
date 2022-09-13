fbe5dd136e6b9dd85d1864b7407205c5889f0196
Isaac Rendon <isaacrendon7@gmail.com>
2022-09-13 15:23:50 -0400

    Rest Pattern

--- script.js
+++ script.js
@@ -44,50 +44,70 @@ const restaurant = {
   },
 };
 
-const arr = [7, 8, 9];
-const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
-console.log(badNewArr);
+// Spread on RIGHT side of assignment operator
+const arr = [1, 2, ...[3, 4]];
 
-const newArr = [1, 2, ...arr];
-console.log(newArr);
+// Rest on LEFT side of assignment operator
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a);
+console.log(b);
+console.log(others);
 
-console.log(...newArr);
-
-const newMenu = [...restaurant.mainMenu, 'Gnocci'];
-console.log(newMenu);
-
-// Copy array
-const mainMenuCopy = [...restaurant.mainMenu];
-console.log(mainMenuCopy);
-
-// Join 2 arrays
-const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
-console.log(menu);
-
-// Iterables are arrays, strings, maps and sets but NOT Objects
-const str = 'Isaac';
-const letters = [...str, ' ', 's.'];
-console.log(letters);
-console.log(...str);
-
-const ingredients = [
-  // prompt("Let's make pasta! Ingredient 1?"),
-  // prompt('Ingredient 2?'),
-  // prompt('Ingredient 3?'),
+// Rest element must be the last element
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
 ];
-console.log(ingredients);
-
-restaurant.orderPasta(...ingredients);
+console.log(pizza, risotto, otherFood);
 
 // Objects
-const newRestaruant = { foudnedIn: 1999, ...restaurant, founder: 'Guiseppe' };
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
 
-console.log(newRestaruant);
+// const arr = [7, 8, 9];
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+// console.log(badNewArr);
 
-const restaurantCopy = { ...restaurant };
-restaurantCopy.name = 'Ristorante Roma';
-console.log(restaurantCopy.name);
-console.log(restaurant.name);
+// const newArr = [1, 2, ...arr];
+// console.log(newArr);
+
+// console.log(...newArr);
+
+// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
+
+// // Copy array
+// const mainMenuCopy = [...restaurant.mainMenu];
+// console.log(mainMenuCopy);
+
+// // Join 2 arrays
+// const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log(menu);
+
+// Iterables are arrays, strings, maps and sets but NOT Objects
+// const str = 'Isaac';
+// const letters = [...str, ' ', 's.'];
+// console.log(letters);
+// console.log(...str);
+
+// const ingredients = [
+//   // prompt("Let's make pasta! Ingredient 1?"),
+//   // prompt('Ingredient 2?'),
+//   // prompt('Ingredient 3?'),
+// ];
+// console.log(ingredients);
+
+// restaurant.orderPasta(...ingredients);
+
+// // Objects
+// const newRestaruant = { foudnedIn: 1999, ...restaurant, founder: 'Guiseppe' };
+
+// console.log(newRestaruant);
+
+// const restaurantCopy = { ...restaurant };
+// restaurantCopy.name = 'Ristorante Roma';
+// console.log(restaurantCopy.name);
+// console.log(restaurant.name);
 
 // restaurant.orderDelivery({
 //   time: '22:30',
