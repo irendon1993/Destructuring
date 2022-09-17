d303782f0123ac66f21ad7624ca0b696d9cee4d1
Isaac Rendon <isaacrendon7@gmail.com>
2022-09-16 20:22:52 -0400

    working with maps

--- script.js
+++ script.js
@@ -44,21 +44,58 @@ const restaurant = {
   },
 };
 
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze,Italy');
+console.log(rest.set(2, 'Lisbon,Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest);
+console.log(rest.size);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+// rest.set([1, 2], 'Test');
+console.log(rest);
+console.log(rest.size);
+
+// Does not work
+// console.log(rest.get([1, 2]));
+
+console.log(rest.get(arr));
+
 //Sets
-const orderSet = new Set(['pizza', 'pasta', 'pizza']);
-console.log(orderSet.size);
-console.log(orderSet.has('pizza'));
-console.log(orderSet.has('bread'));
-orderSet.add('Garlic Bread');
-orderSet.add('Garlic Bread');
-orderSet.delete('pasta');
-console.log(orderSet);
+// const orderSet = new Set(['pizza', 'pasta', 'pizza']);
+// console.log(orderSet.size);
+// console.log(orderSet.has('pizza'));
+// console.log(orderSet.has('bread'));
+// orderSet.add('Garlic Bread');
+// orderSet.add('Garlic Bread');
+// orderSet.delete('pasta');
+// console.log(orderSet);
 
-// Example
-const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+// // Example
+// const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
 
-const staffUnique = [...new Set(staff)];
-console.log(staffUnique);
+// const staffUnique = [...new Set(staff)];
+// console.log(staffUnique);
+
 // Split and Join
 // console.log('a+very+nice+string'.split('+'));
 // console.log('Isaac Rendon'.split(' '));
